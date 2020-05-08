86a0282a94dabbe20c2cff04a210c1031ee751bd
mwostbrock <64874469+mwostbrock@users.noreply.github.com>
2020-05-08 15:45:18 -0400

    added python files from class

---
+++ xr_nc_configure_ipinterface.py
@@ -0,0 +1,34 @@
+from lxml import etree
+from ncclient import manager
+
+if __name__ == "__main__":
+
+    with manager.connect(host='192.168.10.80', port=830, username='admin', password='cisco',
+                         hostkey_verify=False, device_params={'name': 'csr'},
+                         allow_agent=False, look_for_keys=False) as device:
+
+
+        nc_filter = """
+            <config>
+              <interface-configurations xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ifmgr-cfg">
+               <interface-configuration>
+                <active>act</active>
+                <interface-name>Loopback700</interface-name>
+                <interface-virtual/>
+                <ipv4-network xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ipv4-io-cfg">
+                 <addresses>
+                  <primary>
+                   <address>100.100.1.1</address>
+                   <netmask>255.255.255.0</netmask>
+                  </primary>
+                 </addresses>
+                </ipv4-network>
+               </interface-configuration>
+              </interface-configurations>
+            </config>
+        """
+
+        nc_reply = device.edit_config(target='candidate', config=nc_filter)
+        print nc_reply
+        device.commit()
+
